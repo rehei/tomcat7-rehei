1feec3a30f37daad3d239ffd436abc91c9e11699
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2015-10-20 12:39:41 +0200

    Using persistent mirror for Tomcat7 download

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 DESTINATION="dist"
 
 TAG=${TRAVIS_TAG:=7.0.63}
-TOMCAT_URL=http://apache.mirror.iphh.net/tomcat/tomcat-7/v${TAG}/bin/apache-tomcat-${TAG}-windows-x64.zip
+TOMCAT_URL=http://archive.apache.org/dist/tomcat/tomcat-7/v${TAG}/bin/apache-tomcat-${TAG}-windows-x64.zip
 TOMCAT_ZIP=apache-tomcat-${TAG}-windows-x64.zip
 TOMCAT_SOURCE=apache-tomcat-${TAG}
 TOMCAT_DESTINATION=${TOMCAT_SOURCE}-windows-x64-rehei
